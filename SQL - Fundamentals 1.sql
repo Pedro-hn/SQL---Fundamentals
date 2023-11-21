4788fa21edebd51197f074a8247c2e99724668a2
Pedro Henrique <62520348+Pedro-hn@users.noreply.github.com>
2023-11-21 17:07:38 -0300

    Add files via upload

---
+++ SQL - Fundamentals 1.sql
@@ -0,0 +1,9 @@
+-- Author: Pedro Henrique Ribeiro dos Reis - Statistician and Data Scientist
+-- SQL tutorial of principal queries
+-- Selecting all columns from dataset
+select *
+from Person.Address
+
+-- Selecting all columns but just the 10 first rows from dataset
+select top(10) *
+from Person.Address
